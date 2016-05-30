1566ea39fc8dfac1a5a4a07bca1171145fee0594
StuAtGit <stu26code@gmail.com>
2016-05-30 16:39:26 -0700

    Add license

--- js/services/itemService.js
+++ js/services/itemService.js
@@ -1,8 +1,24 @@
 /**
- * Created by stu on 8/30/15.
+ * Copyright 2015-2016 Stuart Smith
  *
- * TODO: load user items asynchronously in this service -
- * compose this object into the user service & begin loading the filelist after login.
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ *
+ * If you modify this Program, or any covered work, by linking or combining
+ * it with the paho MQTT client library (or a modified version of that library),
+ * containing parts covered by the terms of EPL,
+ * the licensors of this Program grant you additional permission to convey the resulting work.
  */
 
 var itemModule = angular.module( "itemModule", ["ng"]);
